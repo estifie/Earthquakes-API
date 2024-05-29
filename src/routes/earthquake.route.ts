97f0ac51c9fb9906aeecd693b2d269579f3a197c
LntAxe <ertusari0@gmail.com>
2024-05-29 19:30:03 +0300

    feat: Add new earthquake endpoints

--- src/routes/earthquake.route.ts
+++ src/routes/earthquake.route.ts
@@ -7,27 +7,46 @@ const router = Router();
 /* GET all earthquakes */
 router.get("/", earthquakeController.getAllEarthquakes);
 
-// /* GET last 1h earthquakes */
-// router.get("/1h", earthquakeController.getLastHourEarthquakes);
+/* GET earthquake by code */
+router.get("/:code/details", earthquakeController.getEarthquakeByCode);
 
-// /* GET last 24h earthquakes */
-// router.get("/24h", earthquakeController.getLastDayEarthquakes);
+/* GET last 30m earthquakes */
+router.get("/30m", earthquakeController.getEarthquakes30Minutes);
 
-// /* GET last 7d earthquakes */
-// router.get("/7d", earthquakeController.getLastWeekEarthquakes);
+/* GET last 1h earthquakes */
+router.get("/1h", earthquakeController.getEarthquakes1Hour);
 
-// /* GET earthquakes above 2.5 magnitude */
-// router.get("/m2.5", earthquakeController.getEarthquakesAbove2_5);
+/* GET last 2h earthquakes */
+router.get("/2h", earthquakeController.getEarthquakes2Hours);
 
-// /* GET earthquakes above 4.5 magnitude */
-// router.get("/m4.5", earthquakeController.getEarthquakesAbove4_5);
+/* GET last 4h earthquakes */
+router.get("/4h", earthquakeController.getEarthquakes4Hours);
 
-// /* GET earthquakes above 6.0 magnitude */
-// router.get("/m6.0", earthquakeController.getEarthquakesAbove6_0);
+/* GET last 12h earthquakes */
+router.get("/12h", earthquakeController.getEarthquakes12Hours);
 
+/* GET last 24h earthquakes */
+router.get("/24h", earthquakeController.getEarthquakes24Hours);
+
+/* GET earthquakes above 2.5 magnitude */
+router.get("/m2.5", earthquakeController.getEarthquakes2_5Magnitude);
+
+/* GET earthquakes above 4.5 magnitude */
+router.get("/m4.5", earthquakeController.getEarthquakes4_5Magnitude);
+
+/* GET earthquakes above 6.0 magnitude */
+router.get("/m6", earthquakeController.getEarthquakes6Magnitude);
+
+/* GET Latest earthquake */
+router.get("/latest", earthquakeController.getLatestEarthquake);
+
+/* POST Create a new earthquake */
 router.post("/", authenticateApiKey, validateCreateEarthquake, earthquakeController.createEarthquake);
-router.get("/admin", authenticateApiKey, earthquakeController.getAllEarthquakesAdmin);
+
+/* GET Fetch earthquakes from external API */
 router.get("/admin/fetch", authenticateApiKey, earthquakeController.fetchEarthquakes);
+
+/* DELETE Delete an earthquake by id */
 router.delete("/:id", authenticateApiKey, earthquakeController.deleteEarthquake);
 
 export default router;
